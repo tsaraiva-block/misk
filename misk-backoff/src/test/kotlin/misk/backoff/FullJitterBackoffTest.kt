f90b8a1926e948dc2c7c9cbfca37e3314e2c10f0
Tarcio Saraiva <tsaraiva@block.xyz>
2026-08-25 11:33:16 +1000

    use correct class for testing

--- misk-backoff/src/test/kotlin/misk/backoff/FullJitterBackoffTest.kt
+++ misk-backoff/src/test/kotlin/misk/backoff/FullJitterBackoffTest.kt
@@ -7,10 +7,7 @@ import org.junit.jupiter.api.Test
 class FullJitterBackoffTest {
   @Test
   fun fullJitteredBackoff() {
-    val backoff =
-      ExponentialBackoff(Duration.ofMillis(10), Duration.ofSeconds(1)) { curDelayMs: Long ->
-        Duration.ofMillis(curDelayMs / 2 + 1)
-      }
+    val backoff = FullJitterBackoff({ Duration.ofMillis(10) }, { Duration.ofSeconds(1) })
 
     assertThat(backoff.nextRetry().toMillis()).isBetween(10L, 20L + 10L)
     assertThat(backoff.nextRetry().toMillis()).isBetween(20L, 30L + 20L)
